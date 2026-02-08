78778a655fdb3b67411f74c46a3022bfed351997
Isaac Lee <legoae86@gmail.com>
2026-02-08 16:46:45 -0500

    comments

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -5,9 +5,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /*
-these will be marked by numbers
-and the related indexing will be
-corresponidng to the file inputted in order
+This module will contain the File Handler object which works when this object is
+called and taking a argument "data directory". By default it will take the parameter
+and call its private function, putFileInArray, which takes the directory name and
+create a file object in order to store those names. Then proceeding the file type array
+it will put each element inside the parser.contents array list storing the string value.
+Immediately after it will call readAndParseFiles which will for the size of the ArrayList
+ and it will call a local string builder and continuously add lines of each .txt file
+ and concatenate it in the StringBuilder object. Once it reaches the end of a .txt file
+it will add it to the contents. Contents will be sorted in order in which ever order
+ it is in the Data folder.
  */
 class FileHandler {
     private ArrayList<String> fileNames;
@@ -16,7 +23,7 @@ class FileHandler {
     public FileHandler() throws IOException {
         fileNames = new ArrayList<>();
         contents = new ArrayList<>();
-        putFilesInArray(new File("data"));
+        putFilesInArray(new File("Data"));
         readAndParseFiles();
     }
 
