ae871857e5d362761de12cc14869d1e0702bcbff
Isaac Lee <legoae86@gmail.com>
2026-02-08 18:07:11 -0500

    test

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -11,8 +11,8 @@ and call its private function, putFileInArray, which takes the directory name an
 create a file object in order to store those names. Then proceeding the file type array
 it will put each element inside the parser.contents array list storing the string value.
 Immediately after it will call readAndParseFiles which will for the size of the ArrayList
- and it will call a local string builder and continuously add lines of each .txt file
- and concatenate it in the StringBuilder object. Once it reaches the end of a .txt file
+and it will call a local string builder and continuously add lines of each .txt file
+and concatenate it in the StringBuilder object. Once it reaches the end of a .txt file
 it will add it to the contents. Contents will be sorted in order in which ever order
  it is in the Data folder.
  */
---
+++ src/test/java/FileHandlerTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileHandlerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getContentsSize() {
+    }
+
+    @Test
+    void getFileNamesSize() {
+    }
+
+    @Test
+    void getFileNames() {
+    }
+
+    @Test
+    void getFileContent() {
+    }
+}
